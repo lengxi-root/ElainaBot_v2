d5bffebc4a4f6459415dcd7d86c960e7130efc74
m0_37157335 <serfend@foxmail.com>
2026-05-21 00:21:25 +0800

    Update bot_restarter.py

--- bot_restarter.py
+++ bot_restarter.py
@@ -1,10 +1,4 @@
-﻿import os
-import subprocess
-import sys
-import time
-from contextlib import suppress
-
-
+import os, sys, time, subprocess
 def main():
     time.sleep(3)
     main_path = r"main.py"
@@ -12,10 +6,8 @@ def main():
     subprocess.Popen([sys.executable, main_path], creationflags=getattr(subprocess, "CREATE_NEW_CONSOLE", 0),  # 旧版Python没有这个字段，进行兼容
                      cwd=os.path.dirname(main_path))
     time.sleep(1)
-    with suppress(Exception):
-        os.remove(__file__)
+    try: os.remove(__file__)
+    except: pass
     sys.exit(0)
-
-
 if __name__ == "__main__":
     main()
